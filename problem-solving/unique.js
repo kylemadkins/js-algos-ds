298068e8b49ac44522e3266bc47f2b9729ad99a7
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-03-19 15:52:27 -0500

    Add comment about instructor's solution using multiple pointers

--- problem-solving/unique.js
+++ problem-solving/unique.js
@@ -13,6 +13,9 @@ function countUniqueValues(arr) {
   return count;
 }
 
+// Instructor gives an example of using multiple pointers to
+// modify the array as you go, but that's unnecessary
+
 console.log([1, 1, 1, 1, 1, 2], countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
 console.log([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13], countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
 console.log([], countUniqueValues([])); // 0
