c84cf3e30d9c32efe98cdceee4b0d1ba85caf5ad
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-03-19 15:51:22 -0500

    Add solution for counting unique values in a sorted array

---
+++ problem-solving/unique.js
@@ -0,0 +1,19 @@
+function countUniqueValues(arr) {
+  if (arr.length < 1) {
+    return 0;
+  }
+  let current = arr[0];
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (current !== arr[i]) {
+      current = arr[i];
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log([1, 1, 1, 1, 1, 2], countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
+console.log([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13], countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+console.log([], countUniqueValues([])); // 0
+console.log([-2, -1, -1, 0, 1], countUniqueValues([-2, -1, -1, 0, 1])); // 4
